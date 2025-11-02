de9b4a82b57da63c623cf0d1f0d5bc44ff9344c7
AkshayTheFintechGuy <akshaytax2014@gmail.com>
2025-11-02 20:43:37 +0530

    sitemap and robottext

---
+++ next-sitemap.config.ts
@@ -0,0 +1,9 @@
+import type { IConfig } from 'next-sitemap'
+
+const config: IConfig = {
+  siteUrl: 'https://vidurbccsmagzine.vercel.app', // your live domain
+  generateRobotsTxt: true, // optional but recommended
+  sitemapSize: 7000,
+}
+
+export default config
